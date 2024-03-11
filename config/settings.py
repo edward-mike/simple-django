fee955bca5939a816073939c578d5642855dc483
Edward Mike <greenpython.developer@gmail.com>
2024-03-11 21:09:03 +0000

    ALLOWED_HOST modified with EC2 instance address

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k7%inimlb@&&dz@$#jmdfvst5fsb3_@j^z5u5%th*%9!t39f7*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost","0.0.0.0","127.0.0.1"]
+ALLOWED_HOSTS = ["13.60.19.111","0.0.0.0","127.0.0.1"]
 
 
 # Application definition
